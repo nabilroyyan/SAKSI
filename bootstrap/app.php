1c12961ad6b5b8b67ac3552578af130fc9579620
vrayoga <vrayogal227@gmail.com>
2025-06-10 02:44:44 +0700

    menambah mobile dekstop

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -17,6 +17,7 @@ return Application::configure(basePath: dirname(__DIR__))
             'permission' => \Spatie\Permission\Middleware\PermissionMiddleware::class,
             'role_or_permission' => \Spatie\Permission\Middleware\RoleOrPermissionMiddleware::class,
             'role_permission' => \App\Http\Middleware\RolePermissionMiddleware::class,
+            'LaravelPwa' => \Ladumor\LaravelPwa\LaravelPwa::class,
         ]);
 
     })
